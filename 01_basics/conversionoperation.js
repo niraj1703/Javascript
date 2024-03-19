468688b336b2aabf228acf5e991d205e17e5b2fe
niraj1703 <131793238+niraj1703@users.noreply.github.com>
2024-03-19 10:15:35 +0000

    conversion of datatypes

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,34 @@
+//let score = "33"
+// let score = "3swrrew"
+// let score = "true"
+// let score = "niraj"
+// let score = "niraj"
+// let score = "niraj"
+// let score = "niraj"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+//let isLoggedIn = "niraj"
+//let isLoggedIn = "1"
+//let isLoggedIn = ""
+//let isLoggedIn = 0
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log( booleanisLoggedIn);
+//console.log(typeof (booleanisLoggedIn));
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 13273372
+let accountEmail = "hitesh@google.com"
+var accountPassward="123456789"
+accountCity = "jaipur"
+let accountState;
+// accountId = 2 // not allowed
+accountEmail = "hc@hc.com"
+accountPassward= "232833443"
+accountCity ="Bangluru"
+console.log([accountId,accountEmail, accountPassward,accountCity,accountState])
