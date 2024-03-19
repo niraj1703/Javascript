a5b7e7e01eedf4f627ae6d53e62393f0adbf6a91
niraj1703 <131793238+niraj1703@users.noreply.github.com>
2024-03-19 14:37:42 +0000

    commit

---
+++ 01_basics/string.js
@@ -0,0 +1,36 @@
+const name = "hitesh "
+const repoCount = 50
+const value = "points"
+// console.log(name +  repoCount +  " points");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount} ${value}`);
+
+const gameName = new String('hitesh-hc-com')
+
+ //console.log(gameName[1]);
+ // console.log(gameName .__proto__);
+  //console.log( __proto__ )
+
+ //console.log(gameName.length);
+ //console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('t'));
+
+//const newString = gameName.substring(0, 5)
+//console.log(newString);
+
+//const anotherString = gameName.slice(-1, 4)
+//console.log(anotherString);
+
+//const newStringOne = "             hitesh    "
+//console.log(newStringOne);
+//console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+//console.log(url.replace('%20', '-'))
+
+//console.log(url.includes('%23'))
+//console.log(url+('%23'))
+
+console.log(gameName.split('-'));
