f5e8e1bc34fb293971515a4ac762263e0957f5d6
niraj1703 <131793238+niraj1703@users.noreply.github.com>
2024-03-19 15:49:30 +0000

    Array

---
+++ 02_basic/array.js
@@ -0,0 +1,45 @@
+//Creating arrays
+//we can store various data types — strings, numbers, objects, and even other arrays.
+//we do not have to limit ourselves to storing only numbers in one array, and in another only strings.
+const myArr = [0, 1, 2, 5, 4, 5, "bread", "milk","tree", 795, [0, 1, 2]]
+const myHeors = ["shaktiman", "naagraj"]
+const myArr2 = new Array(1, 2, 3, 4)
+ 
+//console.log(myArr[10]);
+
+//console.log(myArr.length);     //you find out the length (in characters) of a string — by using the length
+
+
+// Array methods
+
+//myHeors.push("niraj")
+//myHeors.push(6)
+//myHeors.push(9)
+//myHeors.push(6)
+//myHeors.unshift(6)
+// myHeors.pop()
+ //myHeors.shift(6)
+ // myHeors.push(7)
+// console.log(myHeors)
+
+ //console.log(myHeors.includes(9));
+ //console.log(myArr2.indexOf(3));
+
+ //const newArr = myArr.join()
+
+ //console.log(myArr);
+ //console.log( newArr);
+
+
+// slice, splice
+ //console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+//console.log(myn1);
+//console.log("B ", myArr);
+
+
+//const myn2 = myArr.splice(1, 3)
+//console.log("C ", myArr);
+//console.log(myn2);
